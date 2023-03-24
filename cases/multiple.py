9acf2fcee66010322efeab6ab563858589304183
楊德倫 <telunyang@gmail.com>
2023-03-25 00:44:26 +0800

    取得倍數

---
+++ cases/multiple.py
@@ -0,0 +1,19 @@
+'''
+在限定範圍內，只印出某個數字的倍數
+'''
+
+# 印出 3 的倍數
+for i in range(1, 101):
+    if i % 3 == 0:
+        print(i, end=" ")
+
+# print() 不加參數，單純換行
+print()
+
+# 印出 50 個 = 號
+print("=" * 50)
+
+# 印出 2 跟 3 的公倍數
+for i in range(1, 101):
+    if i % 2 == 0 and i % 3 == 0:
+        print(i, end=" ")

6ae8ae9c7800bb6fdb6d279ef66c4bb2e17b1f07
Darren Yang <telunyang@gmail.com>
2023-10-17 20:27:47 +0800

    加入變聲範例

--- cases/google_lady_sound.py
+++ cases/google_lady_sound.py
@@ -10,11 +10,9 @@ https://www.onlinewebtoolkit.com/url-encode-decode
 在 Terminal 使用 curl 指令下載 mp3:
 curl -X GET "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=zh-TW&q=%E4%BD%A0%E7%9A%84%E8%87%AA%E8%A8%82%E6%96%87%E5%AD%97" -o ./test.mp3
 '''
-# 子程序
 import subprocess
-
-# 將人類可讀的文字，轉換成瀏覽器可讀的網址格式
 from urllib.parse import quote
+import os, time
 
 
 '''
@@ -23,7 +21,7 @@ from urllib.parse import quote
 # 設定給 google 小姐發音的文字
 words = '我的優點就是帥，缺點就是帥得不明顯'
 
-# 轉成符合 url 格式的文字
+# # 轉成符合 url 格式的文字
 encode_url = quote(words)
 
 # 定義指令
@@ -89,7 +87,14 @@ else:
 
 '''
 多句下載
+
+參考連結:
+https://gist.github.com/JT5D/a2fdfefa80124a06f5a9
 '''
+# 如果沒有自訂的資料夾，則自動新增
+if not os.path.exists('mp3'):
+    os.makedirs('mp3')
+
 # 設定多個給 google 小姐發音的句子
 list_words = [
     '人生短短幾個秋啊',
@@ -99,7 +104,7 @@ list_words = [
 ]
 
 # 把每一句都下載成 mp3
-for words in list_words:
+for index, words in enumerate(list_words):
     # 轉成符合 url 格式的文字
     encode_url = quote(words)
 
@@ -108,28 +113,28 @@ for words in list_words:
         'curl', 
         '-X', 
         'GET', 
-        f'https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=zh-TW&q={encode_url}',
+        f'https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q={encode_url}',
         '-o',
-        f'./{words}.mp3'
+        f'./mp3/{index}.mp3'
     ]
 
     # 執行指令，回傳 Process 物件，其中的屬性 returncode == 0 代表成功
     std_output = subprocess.run(cmd)
     if std_output.returncode == 0:
-        print(f'[{words}] 下載成功')
+        print(f'[{index}] 下載成功')
     else:
-        print(f'[{words}] 下載失敗')
+        print(f'[{index}] 下載失敗')
 
     cmd = [
         './ffmpeg/bin/ffmpeg.exe', # 左邊是 Windows 指令。MacOS: ./ffmpeg
         '-i',
-        f'./{words}.mp3',
+        f'./mp3/{index}.mp3',
         '-filter:a',
-        'atempo=1.5',
-        f'./{words}_atempo.mp3'
+        'atempo=1.5', # 'asetrate=44100*0.4,atempo=1.5'
+        f'./mp3/{index}_altered.mp3'
     ]
     std_output = subprocess.run(cmd)
     if std_output.returncode == 0:
-        print(f'[{words}] 轉換成功')
+        print(f'[{index}_atempo] 轉換成功')
     else:
-        print(f'[{words}] 轉換失敗')
+        print(f'[{index}_atempo] 轉換失敗')

db79a4943c56a28de6d762d30b098f8e5a7c4561
Darren Yang <telunyang@gmail.com>
2023-10-17 21:41:26 +0800

    更新教材

--- examples/7-3.py
+++ examples/7-3.py
@@ -5,11 +5,26 @@ Logging 設定
 '''
 # 基本設定
 logger = logging.getLogger('my_app')
+
+# 設定等級
+logger.setLevel(logging.INFO)
+
+# 設定輸出格式
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', "%Y-%m-%d %H:%M:%S")
+
+# 儲存在 log 當中的事件處理器
 fileHandler = logging.FileHandler('my_app.log', mode='a', encoding='utf-8') # a: append, w: write
 fileHandler.setFormatter(formatter)
-logger.setLevel(logging.INFO)
+
+# 輸出在終端機介面上的事件處理器
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+# 加入事件
+logger.addHandler(console_handler)
 logger.addHandler(fileHandler)
+
+
 
 # 基本範例
 for x in range(0, 11):
